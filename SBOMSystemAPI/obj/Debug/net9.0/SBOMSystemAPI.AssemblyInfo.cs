9c294672ac2ebc16bba449a38e85982022ad2c6a
kota_yamada <k_yamada@vivo-system.co.jp>
2025-06-26 14:42:37 +0900

    画面No.1の項目整理

--- SBOMSystemAPI/obj/Debug/net9.0/SBOMSystemAPI.AssemblyInfo.cs
+++ SBOMSystemAPI/obj/Debug/net9.0/SBOMSystemAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SBOMSystemAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc53f2d9340929617402d9b2514b7608fa6645f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b34dba35f21763690896c87fcba0457967229a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SBOMSystemAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SBOMSystemAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
