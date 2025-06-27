b42903bb994007a4d878dac9dfb4d0cd3d830216
kota_yamada <k_yamada@vivo-system.co.jp>
2025-06-27 14:27:47 +0900

    画面No.1ページデザインの変更

--- SBOMSystemMockup/obj/Debug/net9.0/SBOMSystemMockup.AssemblyInfo.cs
+++ SBOMSystemMockup/obj/Debug/net9.0/SBOMSystemMockup.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SBOMSystemMockup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2f3229407b2ce9b095552a15e35cc6f2f3af2ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9064b39447510e7f385f205fcf4ab5d2a1cf6e1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SBOMSystemMockup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SBOMSystemMockup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
