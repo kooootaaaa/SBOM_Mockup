9064b39447510e7f385f205fcf4ab5d2a1cf6e1c
kota_yamada <k_yamada@vivo-system.co.jp>
2025-06-27 14:08:04 +0900

    画面No.4部品取得画面のオプション選択仕様を実装

--- SBOMSystemMockup/obj/Debug/net9.0/SBOMSystemMockup.AssemblyInfo.cs
+++ SBOMSystemMockup/obj/Debug/net9.0/SBOMSystemMockup.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SBOMSystemMockup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97c49b4c7ab8925e5686af3e15e47a10803be9d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2f3229407b2ce9b095552a15e35cc6f2f3af2ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("SBOMSystemMockup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SBOMSystemMockup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
